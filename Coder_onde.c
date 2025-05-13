057651b955ef1de7178b3f0148f0b34f8ee1bc92
Totol <91608984+JambonPates@users.noreply.github.com>
2025-05-13 18:57:55 +0200

    Echantillonage fonctionnel

--- Coder_onde.c
+++ Coder_onde.c
@@ -2,14 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 
-float calculerValOndeEnUnPoint(int* ampli1, int* ampli2, int* freq1, int* freq2, int* temps, int* dephas);
-void EntrerDonnees(int* ampli1, int* ampli2, int* freq1, int* freq2, int* temps, int* dephas);
+#define TempsDeSimulation 5.0
+
+float calculerValOndeEnUnPoint(int* ampli1, int* ampli2, int* freq1, int* freq2, float* temps, int* dephas);
+void EntrerDonnees(int* ampli1, int* ampli2, int* freq1, int* freq2, float* temps, int* dephas);
+FILE* ouvrirFichierTXT();
+void fermerFichierTXT(FILE* pointeurFichier);
 
 
 //----------------------------------------------------------------------------------------------------------
 
 
-void EntrerDonnees(int* ampli1, int* ampli2, int* freq1, int* freq2, int* temps, int* dephas){
+void EntrerDonnees(int* ampli1, int* ampli2, int* freq1, int* freq2, float* temps, int* dephas){
 
         printf("/nEntrer Valeur Amplitude A : ");
         scanf("%d", ampli1);
@@ -22,8 +26,8 @@ void EntrerDonnees(int* ampli1, int* ampli2, int* freq1, int* freq2, int* temps,
         printf("/nEntrer Valeur frequence g : ");
         scanf("%d", freq2);
 
-        printf("/nEntrer Valeur temps t : ");
-        scanf("%d", temps);
+        // printf("/nEntrer Valeur temps t : ");
+        // scanf("%d", temps);
 
         printf("/nEntrer Valeur déphasage phi1 : ");
         scanf("%d", dephas);
@@ -31,18 +35,18 @@ void EntrerDonnees(int* ampli1, int* ampli2, int* freq1, int* freq2, int* temps,
 }
 
 
-float calculerValOndeEnUnPoint(int* ampli1, int* ampli2, int* freq1, int* freq2, int* temps, int* dephas){
+float calculerValOndeEnUnPoint(int* ampli1, int* ampli2, int* freq1, int* freq2, float* temps, int* dephas){
 
     return (*ampli1) * cos(2 * 3.14 * (*temps)) + (*ampli2) * cos((2 * 3.14 * (*temps)) + (*dephas));
 
 }
 
-FILE* ouvrirFichierTXT(){
+FILE* ouvrirFichierTXT(char* dataLink){
 
     FILE *f;
     char ch; 
 
-    f = fopen("data.txt", "w");
+    f = fopen(dataLink, "w");
  
     if(f==NULL){
         printf("Erreur lors de l'ouverture d'un fichier");
@@ -59,31 +63,91 @@ void fermerFichierTXT(FILE* pointeurFichier){
 }
 
 
+void Echantillonnage(FILE* data2, int* TauxEchantillonage, int* ampli1, int* ampli2, int* freq1, int* freq2, float* temps, int* dephas){
+
+
+    FILE *data;
+    char ch; 
+
+    data = fopen("data1.txt", "w");
+ 
+    if(data==NULL){
+        printf("Erreur lors de l'ouverture d'un fichier");
+        exit(1);
+    }
+
+    float fbuffer = 0;
+    float* buffer = &fbuffer;
+    float FreqEchant = *TauxEchantillonage;
+    float interval = (TempsDeSimulation) / (*TauxEchantillonage);
+    float* Pinterval = &interval;
+    
+    //printf("taux d'echantillonage = %d\n", *TauxEchantillonage);
+    //printf("Interval = %f\n", interval);
+    
+    fprintf(data, "%f\n", *Pinterval);
+
+    int i = 0;
+    *temps = 0;
+    
+    while(*temps < TempsDeSimulation){
+
+        //printf("T = %f\n", interval);
+        *buffer = calculerValOndeEnUnPoint(ampli1, ampli2, freq1, freq2, temps, dephas);
+        *temps = *temps + interval;
+        i++;
+        //printf("%.2f\n", *buffer);
+        fprintf(data, "%f\n", *buffer);
+
+    }
+
+}
+
+
+int CalculeTauxEchantillon(int* freq1, int* freq2){
+
+    return 2 * 3.14 * (fmax(*(freq1), *(freq2)));
+
+}
+
+
+//----------------------------------------------------------------------------------------------------------
+
 int main(){
 
-    int ampli1 = 0; // A
-    int ampli2 = 0; // B
-    int freq1 = 0; //f
-    int freq2 = 0; // g
-    int temps = 0; // t
+    int ampli1 = 2; // A
+    int ampli2 = 2; // B
+    int freq1 = 3; //f
+    int freq2 = 7; // g
+    float temps = 0; // t
     int dephas = 0; // phi
+    int TauxEchantillonage = 1;
 
     int* Pampli1 = &ampli1; // A
     int* Pampli2 = &ampli2; // B
     int* Pfreq1 = &freq1; //f
     int* Pfreq2 = &freq2; // g
-    int* Ptemps = &temps; // t
+    float* Ptemps = &temps; // t
     int* Pdephas = &dephas; // phi
+    int* PTauxEchantillonage = &TauxEchantillonage;
     
-    char* dataLink =  "data.txt";
 
-    FILE* pFichier;
-    pFichier = ouvrirFichierTXT();
+    char* dataLink1 =  "dataEchantillon.txt";
 
+
+    FILE* pFichierEchant;
+    pFichierEchant = ouvrirFichierTXT(dataLink1);
+
+    TauxEchantillonage = CalculeTauxEchantillon(Pfreq1, Pfreq2);
+
+    Echantillonnage(pFichierEchant, PTauxEchantillonage, Pampli1, Pampli2, Pfreq1, Pfreq2, Ptemps, Pdephas);
+
+    fermerFichierTXT(pFichierEchant);
+    //printf("%.2lf", calculerValOndeEnUnPoint(Pampli1, Pampli2, Pfreq1, Pfreq2, Ptemps, Pdephas));
+
+    
+    
+    //printf("%d\n",*(PTauxEchantillonage));
     //printf("%lf", cos(0.5));
-
-    fermerFichierTXT(pFichier);
-    printf("%.2lf", calculerValOndeEnUnPoint(Pampli1, Pampli2, Pfreq1, Pfreq2, Ptemps, Pdephas));
-
     return 0;
 }
